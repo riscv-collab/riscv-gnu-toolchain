4ad280f90a19b9c51680d61b8b2e8e7c56d2177e
Andrew Waterman <waterman@cs.berkeley.edu>
2014-10-24 00:47:10 -0700

    glibc: fix memcpy link error; improve memcpy

--- glibc/sysdeps/riscv/bits/string.h
+++ glibc/sysdeps/riscv/bits/string.h
@@ -20,38 +20,6 @@ static inline unsigned long __libc_detect_null(unsigned long w)
   return ~(((w & mask) + mask) | w | mask);
 }
 
-#define _HAVE_STRING_ARCH_memcpy 1
-#define __use_memcpy_align(k, d, s, n) \
-  (__builtin_constant_p(n) && (n) % (k) == 0 && (n) <= 64 && \
-   __alignof__(*(d)) >= (k) && __alignof__(*(s)) >= (k))
-#define memcpy(d, s, n) \
-  (__use_memcpy_align(8, d, s, n) ? __memcpy_align8(d, s, n) : \
-   __use_memcpy_align(4, d, s, n) ? __memcpy_align4(d, s, n) : \
-   __memcpy_g(d, s, n))
-
-#define __declare_memcpy_align(size, type) \
-  static inline void *__memcpy_align ## size(void *__restrict __dest, \
-                        __const void *__restrict __src, size_t __n) { \
-    type *__d = (type*)__dest; \
-    const type *__s = (const type*)__src, *__e = (const type*)(__src + __n); \
-    while (__s < __e) { \
-      type __t = *__s; \
-      __d++, __s++; \
-      *(__d-1) = __t; \
-    } \
-    return __dest; \
-  }
-__declare_memcpy_align(8, long long)
-__declare_memcpy_align(4, int)
-
-#ifdef _LIBC
-extern void *__memcpy_g (void *__restrict __dest,
-                         __const void *__restrict __src, size_t __n);
-libc_hidden_proto (__memcpy_g)
-#else
-# define __memcpy_g memcpy
-#endif
-
 #endif /* __GNUC__ && !__cplusplus */
 
 #endif /* bits/string.h */
--- glibc/sysdeps/riscv/memcpy.c
+++ glibc/sysdeps/riscv/memcpy.c
@@ -1,10 +1,7 @@
 #include <string.h>
 #include <stdint.h>
 
-#undef memcpy
-#undef __memcpy_g
-
-void* __memcpy_g(void* aa, const void* bb, size_t n)
+void* __memcpy(void* aa, const void* bb, size_t n)
 {
   #define BODY(a, b, t) { \
     t tt = *b; \
@@ -18,7 +15,7 @@ void* __memcpy_g(void* aa, const void* bb, size_t n)
   uintptr_t msk = sizeof(long)-1;
   if (__builtin_expect(((uintptr_t)a & msk) != ((uintptr_t)b & msk) || n < sizeof(long), 0))
   {
-foo:
+small:
     if (__builtin_expect(a < end, 1))
       while (a < end)
         BODY(a, b, char);
@@ -29,37 +26,45 @@ foo:
     while ((uintptr_t)a & msk)
       BODY(a, b, char);
 
-  long* __restrict__ la = (long*)a;
-  const long* __restrict__ lb = (const long*)b;
+  long* la = (long*)a;
+  const long* lb = (const long*)b;
   long* lend = (long*)((uintptr_t)end & ~msk);
 
   if (__builtin_expect(la < lend-8, 0))
   {
     while (la < lend-8)
     {
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
-      *la++ = *lb++;
+      long b0 = *lb++;
+      long b1 = *lb++;
+      long b2 = *lb++;
+      long b3 = *lb++;
+      long b4 = *lb++;
+      long b5 = *lb++;
+      long b6 = *lb++;
+      long b7 = *lb++;
+      long b8 = *lb++;
+      *la++ = b0;
+      *la++ = b1;
+      *la++ = b2;
+      *la++ = b3;
+      *la++ = b4;
+      *la++ = b5;
+      *la++ = b6;
+      *la++ = b7;
+      *la++ = b8;
     }
     if (la == lend)
-      goto bar;
+      goto maybe_done;
   }
 
   do BODY(la, lb, long) while (la < lend);
 
-bar:
+maybe_done:
   a = (char*)la;
   b = (const char*)lb;
   if (__builtin_expect(a < end, 0))
-    goto foo;
+    goto small;
   return aa;
 }
-libc_hidden_def (__memcpy_g)
-strong_alias (__memcpy_g, memcpy)
+weak_alias (__memcpy, memcpy)
 libc_hidden_builtin_def (memcpy)
