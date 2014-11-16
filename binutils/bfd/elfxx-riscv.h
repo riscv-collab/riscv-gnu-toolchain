e1cf36ae70e7ebd5819ad0d792f99a255a27191a
Andrew Waterman <waterman@cs.berkeley.edu>
2014-11-15 17:41:17 -0800

    binutils: favor elfnn-riscv.c over elfxx-riscv.c
    Since a lot of code is dependent on the word size, it's both simpler and
    more efficient to put that code in elfnn-riscv.c.  Only code that does not
    depend on the word size remains in elfxx-riscv.c.

--- binutils/bfd/elfxx-riscv.h
+++ binutils/bfd/elfxx-riscv.h
@@ -24,75 +24,11 @@
 #include "elf/common.h"
 #include "elf/internal.h"
 
-extern enum elf_reloc_type_class
-riscv_reloc_type_class (const struct bfd_link_info *,
-			const asection *,
-			const Elf_Internal_Rela *);
-
 extern reloc_howto_type *
 riscv_reloc_name_lookup (bfd *, const char *);
-
-extern struct bfd_link_hash_table *
-riscv_elf_link_hash_table_create (bfd *);
 
 extern reloc_howto_type *
 riscv_reloc_type_lookup (bfd *, bfd_reloc_code_real_type);
 
-extern void
-riscv_elf_copy_indirect_symbol (struct bfd_link_info *,
-				struct elf_link_hash_entry *,
-				struct elf_link_hash_entry *);
-
-extern bfd_boolean
-riscv_elf_create_dynamic_sections (bfd *, struct bfd_link_info *);
-
-extern bfd_boolean
-riscv_elf_check_relocs (bfd *, struct bfd_link_info *,
-			asection *, const Elf_Internal_Rela *);
-
-extern bfd_boolean
-riscv_elf_adjust_dynamic_symbol (struct bfd_link_info *,
-				 struct elf_link_hash_entry *);
-
-extern bfd_boolean
-riscv_elf_size_dynamic_sections (bfd *, struct bfd_link_info *);
-
-extern bfd_boolean
-riscv_elf_relocate_section (bfd *, struct bfd_link_info *,
-			    bfd *, asection *,
-			    bfd_byte *, Elf_Internal_Rela *,
-			    Elf_Internal_Sym *,
-			    asection **);
-
-extern asection *
-riscv_elf_gc_mark_hook (asection *,
-			struct bfd_link_info *,
-			Elf_Internal_Rela *,
-			struct elf_link_hash_entry *,
-			Elf_Internal_Sym *);
-
-extern bfd_boolean
-riscv_elf_gc_sweep_hook (bfd *, struct bfd_link_info *,
-			  asection *, const Elf_Internal_Rela *);
-
-extern bfd_vma
-riscv_elf_plt_sym_val (bfd_vma, const asection *, const arelent *);
-
-extern void
-riscv_info_to_howto_rela (bfd *, arelent *, Elf_Internal_Rela *);
-
-extern bfd_boolean
-riscv_elf_finish_dynamic_symbol (bfd *,
-				 struct bfd_link_info *,
-				 struct elf_link_hash_entry *,
-				 Elf_Internal_Sym *);
-
-extern bfd_boolean
-riscv_elf_finish_dynamic_sections (bfd *, struct bfd_link_info *);
-
-extern bfd_boolean
-_bfd_riscv_elf_merge_private_bfd_data (bfd *, bfd *);
-
-extern bfd_boolean
-_bfd_riscv_relax_section (bfd *, asection *, struct bfd_link_info *,
-			  bfd_boolean *);
+extern reloc_howto_type *
+riscv_elf_rtype_to_howto (unsigned int r_type);
