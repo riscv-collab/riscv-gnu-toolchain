riscv-collab/riscv-gnu-toolchain

commit 1ee4364dc4d9de0713b411fbd334690eb4b2ea86

binutils: clean up style
This doesn't fix all the problems, but does clean up many of them to make
the code conform better to the GNU style.

binutils/include/elf/riscv.h
@@ -1,5 +1,5 @@
 /* RISC-V ELF support for BFD.
-   Copyright 2011-2014 Free Software Foundation, Inc.
+   Copyright 2011-2015 Free Software Foundation, Inc.
 
    Contributed by Andrw Waterman <waterman@cs.berkeley.edu> at UC Berkeley.
    Based on MIPS ELF support for BFD, by Ian Lance Taylor.
@@ -17,9 +17,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
-   MA 02110-1301, USA.  */
+   along with this program; see the file COPYING3. If not,
+   see <http://www.gnu.org/licenses/>.  */
 
 /* This file holds definitions specific to the RISCV ELF ABI.  Note
    that most of this is not actually implemented by BFD.  */
@@ -83,7 +82,7 @@ END_RELOC_NUMBERS (R_RISCV_max)
 /* File may contain compressed instructions.  */
 #define EF_RISCV_RVC 0x0001
 
-/* Custom flag definitions. */
+/* Custom flag definitions.  */
 
 #define EF_RISCV_EXT_MASK 0xffff
 #define EF_RISCV_EXT_SH 16
@@ -114,27 +113,27 @@ static const struct riscv_extension_entry riscv_extension_map[] =
   {"Xhwacha", E_RISCV_EXT_Xhwacha},
 };
 
-/* Given an extension name, return an elf flag. */
+/* Given an extension name, return an elf flag.  */
 
-static inline const char* riscv_elf_flag_to_name(unsigned int flag)
+static inline const char *riscv_elf_flag_to_name (unsigned int flag)
 {
   unsigned int i;
 
-  for (i=0; i<sizeof(riscv_extension_map)/sizeof(riscv_extension_map[0]); i++)
+  for (i = 0; i < ARRAY_SIZE (riscv_extension_map); ++i)
     if (riscv_extension_map[i].flag == flag)
       return riscv_extension_map[i].name;
 
   return NULL;
 }
 
-/* Given an elf flag, return an extension name. */
+/* Given an elf flag, return an extension name.  */
 
-static inline unsigned int riscv_elf_name_to_flag(const char* name)
+static inline unsigned int riscv_elf_name_to_flag (const char *name)
 {
   unsigned int i;
 
-  for (i=0; i<sizeof(riscv_extension_map)/sizeof(riscv_extension_map[0]); i++)
-    if (strcmp(riscv_extension_map[i].name, name) == 0)
+  for (i = 0; i < ARRAY_SIZE (riscv_extension_map); ++i)
+    if (strcmp (riscv_extension_map[i].name, name) == 0)
       return riscv_extension_map[i].flag;
 
   return E_RISCV_EXT_Xcustom;

binutils/include/opcode/riscv.h
@@ -1,7 +1,7 @@
 /* riscv.h.  RISC-V opcode list for GDB, the GNU debugger.
    Copyright 2011
    Free Software Foundation, Inc.
-   Contributed by Andrew Waterman 
+   Contributed by Andrew Waterman
 
 This file is part of GDB, GAS, and the GNU binutils.
 
@@ -26,7 +26,7 @@ Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, US
 #include <stdlib.h>
 #include <stdint.h>
 
-/* RVC fields */
+/* RVC fields. */
 
 #define OP_MASK_CRS2 0x1f
 #define OP_SH_CRS2 2
@@ -46,33 +46,33 @@ typedef uint64_t insn_t;
 
 static inline unsigned int riscv_insn_length (insn_t insn)
 {
-  if ((insn & 0x3) != 3) /* RVC */
+  if ((insn & 0x3) != 3) /* RVC.  */
     return 2;
-  if ((insn & 0x1f) != 0x1f) /* base ISA and extensions in 32-bit space */
+  if ((insn & 0x1f) != 0x1f) /* Base ISA and extensions in 32-bit space.  */
     return 4;
-  if ((insn & 0x3f) == 0x1f) /* 48-bit extensions */
+  if ((insn & 0x3f) == 0x1f) /* 48-bit extensions.  */
     return 6;
-  if ((insn & 0x7f) == 0x3f) /* 64-bit extensions */
+  if ((insn & 0x7f) == 0x3f) /* 64-bit extensions.  */
     return 8;
-  /* longer instructions not supported at the moment */
+  /* Longer instructions not supported at the moment.  */
   return 2;
 }
 
 static const char * const riscv_rm[8] = {
   "rne", "rtz", "rdn", "rup", "rmm", 0, 0, "dyn"
 };
-static const char* const riscv_pred_succ[16] = {
+static const char * const riscv_pred_succ[16] = {
   0,   "w",  "r",  "rw",  "o",  "ow",  "or",  "orw",
   "i", "iw", "ir", "irw", "io", "iow", "ior", "iorw",
 };
 
 #define RVC_JUMP_BITS 11
-#define RVC_JUMP_REACH ((1ULL<<RVC_JUMP_BITS)*RISCV_JUMP_ALIGN)
+#define RVC_JUMP_REACH ((1ULL << RVC_JUMP_BITS) * RISCV_JUMP_ALIGN)
 
 #define RVC_BRANCH_BITS 8
-#define RVC_BRANCH_REACH ((1ULL<<RVC_BRANCH_BITS)*RISCV_BRANCH_ALIGN)
+#define RVC_BRANCH_REACH ((1ULL << RVC_BRANCH_BITS) * RISCV_BRANCH_ALIGN)
 
-#define RV_X(x, s, n) (((x) >> (s)) & ((1<<(n))-1))
+#define RV_X(x, s, n) (((x) >> (s)) & ((1 << (n)) - 1))
 #define RV_IMM_SIGN(x) (-(((x) >> 31) & 1))
 
 #define EXTRACT_ITYPE_IMM(x) \
@@ -230,16 +230,16 @@ static const char* const riscv_pred_succ[16] = {
 #define OP_MASK_VFR		0x1f
 #define OP_SH_VFR		27
 
-#define OP_MASK_IMMNGPR         0x3f
-#define OP_SH_IMMNGPR           20
-#define OP_MASK_IMMNFPR         0x3f
-#define OP_SH_IMMNFPR           26
-#define OP_MASK_IMMSEGNELM      0x7
-#define OP_SH_IMMSEGNELM        29
-#define OP_MASK_CUSTOM_IMM      0x7f
-#define OP_SH_CUSTOM_IMM        25
-#define OP_MASK_CSR             0xfff
-#define OP_SH_CSR               20
+#define OP_MASK_IMMNGPR		0x3f
+#define OP_SH_IMMNGPR		20
+#define OP_MASK_IMMNFPR		0x3f
+#define OP_SH_IMMNFPR		26
+#define OP_MASK_IMMSEGNELM	0x7
+#define OP_SH_IMMSEGNELM	29
+#define OP_MASK_CUSTOM_IMM	0x7f
+#define OP_SH_CUSTOM_IMM	25
+#define OP_MASK_CSR		0xfff
+#define OP_SH_CSR		20
 
 #define X_RA 1
 #define X_SP 2
@@ -258,17 +258,17 @@ static const char* const riscv_pred_succ[16] = {
 #define RISCV_JUMP_BITS RISCV_BIGIMM_BITS
 #define RISCV_JUMP_ALIGN_BITS 1
 #define RISCV_JUMP_ALIGN (1 << RISCV_JUMP_ALIGN_BITS)
-#define RISCV_JUMP_REACH ((1ULL<<RISCV_JUMP_BITS)*RISCV_JUMP_ALIGN)
+#define RISCV_JUMP_REACH ((1ULL << RISCV_JUMP_BITS) * RISCV_JUMP_ALIGN)
 
 #define RISCV_IMM_BITS 12
-#define RISCV_BIGIMM_BITS (32-RISCV_IMM_BITS)
-#define RISCV_IMM_REACH (1LL<<RISCV_IMM_BITS)
-#define RISCV_BIGIMM_REACH (1LL<<RISCV_BIGIMM_BITS)
-#define RISCV_RVC_IMM_REACH (1LL<<6)
+#define RISCV_BIGIMM_BITS (32 - RISCV_IMM_BITS)
+#define RISCV_IMM_REACH (1LL << RISCV_IMM_BITS)
+#define RISCV_BIGIMM_REACH (1LL << RISCV_BIGIMM_BITS)
+#define RISCV_RVC_IMM_REACH (1LL << 6)
 #define RISCV_BRANCH_BITS RISCV_IMM_BITS
 #define RISCV_BRANCH_ALIGN_BITS RISCV_JUMP_ALIGN_BITS
 #define RISCV_BRANCH_ALIGN (1 << RISCV_BRANCH_ALIGN_BITS)
-#define RISCV_BRANCH_REACH (RISCV_IMM_REACH*RISCV_BRANCH_ALIGN)
+#define RISCV_BRANCH_REACH (RISCV_IMM_REACH * RISCV_BRANCH_ALIGN)
 
 /* This structure holds information for a particular instruction.  */
 
@@ -276,7 +276,7 @@ struct riscv_opcode
 {
   /* The name of the instruction.  */
   const char *name;
-  /* The ISA subset name (I, M, A, F, D, Xextension). */
+  /* The ISA subset name (I, M, A, F, D, Xextension).  */
   const char *subset;
   /* A string describing the arguments for this instruction.  */
   const char *args;
@@ -291,27 +291,27 @@ struct riscv_opcode
      INSN_MACRO, then this field is the macro identifier.  */
   insn_t mask;
   /* A function to determine if a word corresponds to this instruction.
-     Usually, this computes ((word & mask) == match). */
-  int (*match_func)(const struct riscv_opcode *op, insn_t word);
+     Usually, this computes ((word & mask) == match).  */
+  int (*match_func) (const struct riscv_opcode *op, insn_t word);
   /* For a macro, this is INSN_MACRO.  Otherwise, it is a collection
      of bits describing the instruction, notably any relevant hazard
      information.  */
   unsigned long pinfo;
 };
 
-#define INSN_WRITE_GPR_D            0x00000001
-#define INSN_WRITE_GPR_RA           0x00000004
-#define INSN_WRITE_FPR_D            0x00000008
-#define INSN_READ_GPR_S             0x00000040
-#define INSN_READ_GPR_T             0x00000080
-#define INSN_READ_FPR_S             0x00000100
-#define INSN_READ_FPR_T             0x00000200
-#define INSN_READ_FPR_R        	    0x00000400
-/* Instruction is a simple alias (I.E. "move" for daddu/addu/or) */
-#define	INSN_ALIAS		    0x00001000
+#define INSN_WRITE_GPR_D	0x00000001
+#define INSN_WRITE_GPR_RA	0x00000004
+#define INSN_WRITE_FPR_D	0x00000008
+#define INSN_READ_GPR_S		0x00000040
+#define INSN_READ_GPR_T		0x00000080
+#define INSN_READ_FPR_S		0x00000100
+#define INSN_READ_FPR_T		0x00000200
+#define INSN_READ_FPR_R		0x00000400
+/* Instruction is a simple alias (I.E. "move" for daddu/addu/or).  */
+#define	INSN_ALIAS		0x00001000
 /* Instruction is actually a macro.  It should be ignored by the
    disassembler, and requires special treatment by the assembler.  */
-#define INSN_MACRO                  0xffffffff
+#define INSN_MACRO		0xffffffff
 
 /* This is a list of macro expanded instructions.
 

binutils/bfd/cpu-riscv.c
@@ -1,5 +1,5 @@
 /* BFD backend for RISC-V
-   Copyright 2011-2014 Free Software Foundation, Inc.
+   Copyright 2011-2015 Free Software Foundation, Inc.
 
    Contributed by Andrew Waterman (waterman@cs.berkeley.edu) at UC Berkeley.
    Based on MIPS target.
@@ -17,9 +17,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
-   MA 02110-1301, USA.  */
+   along with this program; see the file COPYING3. If not,
+   see <http://www.gnu.org/licenses/>.  */
 
 #include "sysdep.h"
 #include "bfd.h"
@@ -70,11 +69,11 @@ enum
 
 static const bfd_arch_info_type arch_info_struct[] =
 {
-  N (64, 64, bfd_mach_riscv64, "riscv:rv64", FALSE, NN(I_riscv64)),
+  N (64, 64, bfd_mach_riscv64, "riscv:rv64", FALSE, NN (I_riscv64)),
   N (32, 32, bfd_mach_riscv32, "riscv:rv32", FALSE, 0)
 };
 
-/* The default architecture is riscv:rv64. */
+/* The default architecture is riscv:rv64.  */
 
 const bfd_arch_info_type bfd_riscv_arch =
 N (64, 64, 0, "riscv", TRUE, &arch_info_struct[0]);

binutils/bfd/elfnn-riscv.c
@@ -1,5 +1,5 @@
 /* RISC-V-specific support for NN-bit ELF.
-   Copyright 2011-2014 Free Software Foundation, Inc.
+   Copyright 2011-2015 Free Software Foundation, Inc.
 
    Contributed by Andrew Waterman (waterman@cs.berkeley.edu) at UC Berkeley.
    Based on TILE-Gx and MIPS targets.
@@ -17,10 +17,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
-   MA 02110-1301, USA.  */
-
+   along with this program; see the file COPYING3. If not,
+   see <http://www.gnu.org/licenses/>.  */
 
 /* This file handles RISC-V ELF targets.  */
 
@@ -109,7 +107,7 @@ struct _bfd_riscv_elf_obj_tdata
   (_bfd_riscv_elf_tdata (abfd)->local_got_tls_type)
 
 #define _bfd_riscv_elf_tls_type(abfd, h, symndx)		\
-  (*((h) != NULL ? &riscv_elf_hash_entry(h)->tls_type		\
+  (*((h) != NULL ? &riscv_elf_hash_entry (h)->tls_type		\
      : &_bfd_riscv_elf_local_got_tls_type (abfd) [symndx]))
 
 #define is_riscv_elf(bfd)				\
@@ -187,7 +185,7 @@ riscv_elf_got_plt_val (bfd_vma plt_index, struct bfd_link_info *info)
 /* The format of the first PLT entry.  */
 
 static void
-riscv_make_plt0_entry(bfd_vma gotplt_addr, bfd_vma addr, uint32_t *entry)
+riscv_make_plt0_entry (bfd_vma gotplt_addr, bfd_vma addr, uint32_t *entry)
 {
   /* auipc  t2, %hi(.got.plt)
      sub    t1, t1, t3               # shifted .got.plt offset + hdr size + 12
@@ -211,7 +209,7 @@ riscv_make_plt0_entry(bfd_vma gotplt_addr, bfd_vma addr, uint32_t *entry)
 /* The format of subsequent PLT entries.  */
 
 static void
-riscv_make_plt_entry(bfd_vma got_address, bfd_vma addr, uint32_t *entry)
+riscv_make_plt_entry (bfd_vma got_address, bfd_vma addr, uint32_t *entry)
 {
   /* auipc  t3, %hi(.got.plt entry)
      l[w|d] t3, %lo(.got.plt entry)(t3)
@@ -457,7 +455,7 @@ riscv_elf_record_got_reference (bfd *abfd, struct bfd_link_info *info,
   if (htab->elf.sgot == NULL)
     {
       if (!riscv_elf_create_got_section (htab->elf.dynobj, info))
-        return FALSE;
+	return FALSE;
     }
 
   if (h != NULL)
@@ -484,7 +482,8 @@ static bfd_boolean
 bad_static_reloc (bfd *abfd, unsigned r_type, struct elf_link_hash_entry *h)
 {
   (*_bfd_error_handler)
-    (_("%B: relocation %s against `%s' can not be used when making a shared object; recompile with -fPIC"),
+    (_("%B: relocation %s against `%s' can not be used when making a shared "
+       "object; recompile with -fPIC"),
       abfd, riscv_elf_rtype_to_howto (r_type)->name,
       h != NULL ? h->root.root.string : "a local symbol");
   bfd_set_error (bfd_error_bad_value);
@@ -1051,7 +1050,7 @@ allocate_dynrelocs (struct elf_link_hash_entry *h, void *inf)
     {
       asection *s;
       bfd_boolean dyn;
-      int tls_type = riscv_elf_hash_entry(h)->tls_type;
+      int tls_type = riscv_elf_hash_entry (h)->tls_type;
 
       /* Make sure this symbol is output as a dynamic symbol.
 	 Undefined weak syms won't yet be marked as dynamic.  */
@@ -2065,17 +2064,17 @@ riscv_elf_relocate_section (bfd *output_bfd, struct bfd_link_info *info,
 		abort ();
 
 	      if (h != NULL)
-	      {
-	        bfd_boolean dyn;
-	        dyn = htab->elf.dynamic_sections_created;
+		{
+		  bfd_boolean dyn;
+		  dyn = htab->elf.dynamic_sections_created;
 
-		if (WILL_CALL_FINISH_DYNAMIC_SYMBOL (dyn, info->shared, h)
-		    && (!info->shared
-			|| !SYMBOL_REFERENCES_LOCAL (info, h)))
+		  if (WILL_CALL_FINISH_DYNAMIC_SYMBOL (dyn, info->shared, h)
+		      && (!info->shared
+			  || !SYMBOL_REFERENCES_LOCAL (info, h)))
 		  {
 		    indx = h->dynindx;
 		  }
-	      }
+		}
 
 	      /* The GOT entries have not been initialized yet.  Do it
 	         now, and emit any relocations.  */
@@ -2096,35 +2095,35 @@ riscv_elf_relocate_section (bfd *output_bfd, struct bfd_link_info *info,
 				  htab->elf.sgot->contents + off);
 		      riscv_elf_append_rela (output_bfd, htab->elf.srelgot, &outrel);
 		      if (indx == 0)
-		        {
+			{
 			  BFD_ASSERT (! unresolved_reloc);
 			  bfd_put_NN (output_bfd,
 				      dtpoff (info, relocation),
-				      (htab->elf.sgot->contents + off +
+				      (htab->elf.sgot->contents + off
 				       RISCV_ELF_WORD_BYTES));
-		        }
+			}
 		      else
-		        {
+			{
 			  bfd_put_NN (output_bfd, 0,
-				      (htab->elf.sgot->contents + off +
+				      (htab->elf.sgot->contents + off
 				       RISCV_ELF_WORD_BYTES));
-		          outrel.r_info = ELFNN_R_INFO (indx, R_RISCV_TLS_DTPRELNN);
-		          outrel.r_offset += RISCV_ELF_WORD_BYTES;
-		          riscv_elf_append_rela (output_bfd, htab->elf.srelgot, &outrel);
-		        }
+			  outrel.r_info = ELFNN_R_INFO (indx, R_RISCV_TLS_DTPRELNN);
+			  outrel.r_offset += RISCV_ELF_WORD_BYTES;
+			  riscv_elf_append_rela (output_bfd, htab->elf.srelgot, &outrel);
+			}
 		    }
 		  else
 		    {
 		      /* If we are not emitting relocations for a
-		         general dynamic reference, then we must be in a
-		         static link or an executable link with the
-		         symbol binding locally.  Mark it as belonging
-		         to module 1, the executable.  */
+			 general dynamic reference, then we must be in a
+			 static link or an executable link with the
+			 symbol binding locally.  Mark it as belonging
+			 to module 1, the executable.  */
 		      bfd_put_NN (output_bfd, 1,
 				  htab->elf.sgot->contents + off);
 		      bfd_put_NN (output_bfd,
 				  dtpoff (info, relocation),
-				  (htab->elf.sgot->contents + off +
+				  (htab->elf.sgot->contents + off
 				   RISCV_ELF_WORD_BYTES));
 		   }
 		}
@@ -2139,7 +2138,7 @@ riscv_elf_relocate_section (bfd *output_bfd, struct bfd_link_info *info,
 				       + off + ie_off;
 		      outrel.r_addend = 0;
 		      if (indx == 0)
-		        outrel.r_addend = tpoff (info, relocation);
+			outrel.r_addend = tpoff (info, relocation);
 		      outrel.r_info = ELFNN_R_INFO (indx, R_RISCV_TLS_TPRELNN);
 		      riscv_elf_append_rela (output_bfd, htab->elf.srelgot, &outrel);
 		    }
@@ -2189,36 +2188,36 @@ riscv_elf_relocate_section (bfd *output_bfd, struct bfd_link_info *info,
 	{
 	case bfd_reloc_ok:
 	  continue;
-      
+
 	case bfd_reloc_overflow:
 	  r = info->callbacks->reloc_overflow
 	    (info, (h ? &h->root : NULL), name, howto->name,
 	     (bfd_vma) 0, input_bfd, input_section, rel->r_offset);
 	  break;
-      
+
 	case bfd_reloc_undefined:
 	  r = info->callbacks->undefined_symbol
 	    (info, name, input_bfd, input_section, rel->r_offset,
 	     TRUE);
 	  break;
-      
+
 	case bfd_reloc_outofrange:
 	  msg = _("internal error: out of range error");
 	  break;
-      
+
 	case bfd_reloc_notsupported:
 	  msg = _("internal error: unsupported relocation error");
 	  break;
-      
+
 	case bfd_reloc_dangerous:
 	  msg = _("internal error: dangerous relocation");
 	  break;
-      
+
 	default:
 	  msg = _("internal error: unknown error");
 	  break;
 	}
-      
+
       if (msg)
 	r = info->callbacks->warning
 	  (info, msg, name, input_bfd, input_section, rel->r_offset);
@@ -2556,7 +2555,7 @@ _bfd_riscv_elf_merge_private_bfd_data (bfd *ibfd, bfd *obfd)
 
       if (bfd_get_arch (obfd) == bfd_get_arch (ibfd)
 	  && (bfd_get_arch_info (obfd)->the_default
-	      || riscv_mach_extends_p (bfd_get_mach (obfd), 
+	      || riscv_mach_extends_p (bfd_get_mach (obfd),
 				       bfd_get_mach (ibfd))))
 	{
 	  if (! bfd_set_arch_mach (obfd, bfd_get_arch (ibfd),
@@ -2588,12 +2587,13 @@ _bfd_riscv_elf_merge_private_bfd_data (bfd *ibfd, bfd *obfd)
 	  !EF_IS_RISCV_EXT_Xcustom (old_flags))
 	{
 	  (*_bfd_error_handler)
-	    (_("%B: uses different e_flags (0x%lx) fields than previous modules (0x%lx)"),
-	    ibfd, (unsigned long) new_flags,
-	    (unsigned long) old_flags);
+	    (_("%B: uses different e_flags (0x%lx) fields than previous "
+	       "modules (0x%lx)"),
+	     ibfd, (unsigned long) new_flags,
+	     (unsigned long) old_flags);
 	  goto fail;
 	}
-      else if (EF_IS_RISCV_EXT_Xcustom(new_flags))
+      else if (EF_IS_RISCV_EXT_Xcustom (new_flags))
 	EF_SET_RISCV_EXT (elf_elfheader (obfd)->e_flags,
 			  EF_GET_RISCV_EXT (old_flags));
     }
@@ -2651,7 +2651,7 @@ riscv_relax_delete_bytes (bfd *abfd, asection *sec, bfd_vma addr, size_t count)
     }
 
   /* Now adjust the global symbols defined in this section.  */
-  symcount = ((symtab_hdr->sh_size / sizeof(ElfNN_External_Sym))
+  symcount = ((symtab_hdr->sh_size / sizeof (ElfNN_External_Sym))
 	      - symtab_hdr->sh_info);
 
   for (i = 0; i < symcount; i++)
@@ -2871,7 +2871,7 @@ _bfd_riscv_relax_section (bfd *abfd, asection *sec,
     {
       asection *sym_sec;
       Elf_Internal_Rela *rel = relocs + i;
-      typeof(&_bfd_riscv_relax_call) relax_func = NULL;
+      typeof (&_bfd_riscv_relax_call) relax_func = NULL;
       int type = ELFNN_R_TYPE (rel->r_info);
       bfd_vma symval;
 
@@ -2970,7 +2970,7 @@ fail:
 
 #define elf_backend_reloc_type_class	     riscv_reloc_type_class
 
-#define bfd_elfNN_bfd_reloc_name_lookup      riscv_reloc_name_lookup
+#define bfd_elfNN_bfd_reloc_name_lookup	     riscv_reloc_name_lookup
 #define bfd_elfNN_bfd_link_hash_table_create riscv_elf_link_hash_table_create
 #define bfd_elfNN_bfd_reloc_type_lookup	     riscv_reloc_type_lookup
 #define bfd_elfNN_bfd_merge_private_bfd_data \
@@ -2985,22 +2985,22 @@ fail:
 #define elf_backend_finish_dynamic_symbol    riscv_elf_finish_dynamic_symbol
 #define elf_backend_finish_dynamic_sections  riscv_elf_finish_dynamic_sections
 #define elf_backend_gc_mark_hook	     riscv_elf_gc_mark_hook
-#define elf_backend_gc_sweep_hook            riscv_elf_gc_sweep_hook
+#define elf_backend_gc_sweep_hook	     riscv_elf_gc_sweep_hook
 #define elf_backend_plt_sym_val		     riscv_elf_plt_sym_val
-#define elf_info_to_howto_rel                NULL
-#define elf_info_to_howto                    riscv_info_to_howto_rela
-#define bfd_elfNN_bfd_relax_section          _bfd_riscv_relax_section
+#define elf_info_to_howto_rel		     NULL
+#define elf_info_to_howto		     riscv_info_to_howto_rela
+#define bfd_elfNN_bfd_relax_section	     _bfd_riscv_relax_section
 
-#define elf_backend_init_index_section	_bfd_elf_init_1_index_section
+#define elf_backend_init_index_section	     _bfd_elf_init_1_index_section
 
-#define elf_backend_can_gc_sections 1
-#define elf_backend_can_refcount 1
-#define elf_backend_want_got_plt 1
-#define elf_backend_plt_readonly 1
-#define elf_backend_plt_alignment 4
-#define elf_backend_want_plt_sym 1
-#define elf_backend_got_header_size (ARCH_SIZE / 8)
-#define elf_backend_rela_normal 1
-#define elf_backend_default_execstack 0
+#define elf_backend_can_gc_sections	1
+#define elf_backend_can_refcount	1
+#define elf_backend_want_got_plt	1
+#define elf_backend_plt_readonly	1
+#define elf_backend_plt_alignment	4
+#define elf_backend_want_plt_sym	1
+#define elf_backend_got_header_size	(ARCH_SIZE / 8)
+#define elf_backend_rela_normal		1
+#define elf_backend_default_execstack	0
 
 #include "elfNN-target.h"

binutils/bfd/elfxx-riscv.c
@@ -1,5 +1,5 @@
 /* RISC-V-specific support for ELF.
-   Copyright 2011-2014 Free Software Foundation, Inc.
+   Copyright 2011-2015 Free Software Foundation, Inc.
 
    Contributed by Andrew Waterman (waterman@cs.berkeley.edu) at UC Berkeley.
    Based on TILE-Gx and MIPS targets.
@@ -17,9 +17,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
-   MA 02110-1301, USA.  */
+   along with this program; see the file COPYING3. If not,
+   see <http://www.gnu.org/licenses/>.  */
 
 #include "sysdep.h"
 #include "bfd.h"
@@ -227,7 +226,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_BRANCH",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_SBTYPE_IMM(-1U),/* dst_mask */
+	 ENCODE_SBTYPE_IMM (-1U),/* dst_mask */
 	 TRUE),			/* pcrel_offset */
 
   /* 20-bit PC-relative jump offset.  */
@@ -245,7 +244,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_JAL",		/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UJTYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_UJTYPE_IMM (-1U),	/* dst_mask */
 	 TRUE),		/* pcrel_offset */
 
   /* 32-bit PC-relative function call (AUIPC/JALR).  */
@@ -260,7 +259,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_CALL",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U) | ((bfd_vma) ENCODE_ITYPE_IMM(-1U) << 32),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U) | ((bfd_vma) ENCODE_ITYPE_IMM (-1U) << 32),	/* dst_mask */
 	 TRUE),			/* pcrel_offset */
 
   /* 32-bit PC-relative function call (AUIPC/JALR).  */
@@ -275,7 +274,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_CALL_PLT",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U) | ((bfd_vma) ENCODE_ITYPE_IMM(-1U) << 32),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U) | ((bfd_vma) ENCODE_ITYPE_IMM (-1U) << 32),	/* dst_mask */
 	 TRUE),			/* pcrel_offset */
 
   /* High 20 bits of 32-bit PC-relative GOT access.  */
@@ -290,7 +289,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_GOT_HI20",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* High 20 bits of 32-bit PC-relative TLS IE GOT access.  */
@@ -305,7 +304,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_TLS_GOT_HI20",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* High 20 bits of 32-bit PC-relative TLS GD GOT reference.  */
@@ -320,7 +319,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_TLS_GD_HI20",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* High 20 bits of 32-bit PC-relative reference.  */
@@ -335,7 +334,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_PCREL_HI20",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U),	/* dst_mask */
 	 TRUE),			/* pcrel_offset */
 
   /* Low 12 bits of a 32-bit PC-relative load or add.  */
@@ -350,7 +349,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_PCREL_LO12_I",/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_ITYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_ITYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* Low 12 bits of a 32-bit PC-relative store.  */
@@ -365,7 +364,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_PCREL_LO12_S",/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_STYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_STYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* High 20 bits of 32-bit absolute address.  */
@@ -380,7 +379,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_HI20",		/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* High 12 bits of 32-bit load or add.  */
@@ -395,7 +394,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_LO12_I",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_ITYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_ITYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* High 12 bits of 32-bit store.  */
@@ -410,7 +409,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_LO12_S",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_STYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_STYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* High 20 bits of TLS LE thread pointer offset.  */
@@ -425,7 +424,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_TPREL_HI20",	/* name */
 	 TRUE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_UTYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_UTYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* Low 12 bits of TLS LE thread pointer offset for loads and adds.  */
@@ -440,7 +439,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_TPREL_LO12_I",/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_ITYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_ITYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* Low 12 bits of TLS LE thread pointer offset for stores.  */
@@ -455,7 +454,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_TPREL_LO12_S",/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_STYPE_IMM(-1U),	/* dst_mask */
+	 ENCODE_STYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
   /* TLS LE thread pointer usage.  */
@@ -652,7 +651,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_RVC_BRANCH",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_RVC_B_IMM(-1U),	/* dst_mask */
+	 ENCODE_RVC_B_IMM (-1U),	/* dst_mask */
 	 TRUE),			/* pcrel_offset */
 
   /* 11-bit PC-relative jump offset.  */
@@ -670,7 +669,7 @@ static reloc_howto_type howto_table[] =
 	 "R_RISCV_RVC_JUMP",	/* name */
 	 FALSE,			/* partial_inplace */
 	 0,			/* src_mask */
-	 ENCODE_RVC_J_IMM(-1U),	/* dst_mask */
+	 ENCODE_RVC_J_IMM (-1U),	/* dst_mask */
 	 TRUE),		/* pcrel_offset */
 };
 
@@ -727,7 +726,7 @@ static const struct elf_reloc_map riscv_reloc_map[] =
 
 reloc_howto_type *
 riscv_reloc_type_lookup (bfd *abfd ATTRIBUTE_UNUSED,
-				 bfd_reloc_code_real_type code)
+			 bfd_reloc_code_real_type code)
 {
   unsigned int i;
 
@@ -741,7 +740,7 @@ riscv_reloc_type_lookup (bfd *abfd ATTRIBUTE_UNUSED,
 
 reloc_howto_type *
 riscv_reloc_name_lookup (bfd *abfd ATTRIBUTE_UNUSED,
-				 const char *r_name)
+			 const char *r_name)
 {
   unsigned int i;
 
@@ -755,9 +754,9 @@ riscv_reloc_name_lookup (bfd *abfd ATTRIBUTE_UNUSED,
 reloc_howto_type *
 riscv_elf_rtype_to_howto (unsigned int r_type)
 {
-  if ((unsigned int)r_type >= ARRAY_SIZE (howto_table))
+  if (r_type >= ARRAY_SIZE (howto_table))
     {
-      (*_bfd_error_handler)(_("unrecognized relocation (0x%x)"), r_type);
+      (*_bfd_error_handler) (_("unrecognized relocation (0x%x)"), r_type);
       bfd_set_error (bfd_error_bad_value);
       return NULL;
     }

binutils/bfd/elfxx-riscv.h
@@ -1,5 +1,5 @@
 /* RISC-V ELF specific backend routines.
-   Copyright 2011-2014 Free Software Foundation, Inc.
+   Copyright 2011-2015 Free Software Foundation, Inc.
 
    Contributed by Andrew Waterman (waterman@cs.berkeley.edu) at UC Berkeley.
    Based on MIPS target.
@@ -17,9 +17,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
-   MA 02110-1301, USA.  */
+   along with this program; see the file COPYING3. If not,
+   see <http://www.gnu.org/licenses/>.  */
 
 #include "elf/common.h"
 #include "elf/internal.h"

commit 6b13b3d31853e92dab8a162ea27ea1b6da3c3989

Add a missing include file
Fixes: 1ee4364dc4d9 ("binutils: clean up style")

binutils/include/elf/riscv.h
@@ -27,6 +27,7 @@
 #define _ELF_RISCV_H
 
 #include "elf/reloc-macros.h"
+#include "libiberty.h"
 
 /* Relocation types.  */
 START_RELOC_NUMBERS (elf_riscv_reloc_type)