5cb73e32c607089b44eb74d546aa2abb4b1b275e
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2015-09-18 23:49:58 -0700

    binutils: bfd: realign R_RISCV_TPREL_HI20

--- binutils/bfd/elfxx-riscv.c
+++ binutils/bfd/elfxx-riscv.c
@@ -415,17 +415,17 @@ static reloc_howto_type howto_table[] =
 	 FALSE),		/* pcrel_offset */
 
   /* High 20 bits of TLS LE thread pointer offset.  */
-  HOWTO (R_RISCV_TPREL_HI20,	/* type */
-	 0,			/* rightshift */
-	 2,			/* size (0 = byte, 1 = short, 2 = long) */
-	 32,	/* bitsize */
-	 FALSE,			/* pc_relative */
-	 0,			/* bitpos */
-	 complain_overflow_signed, /* complain_on_overflow */
-	 bfd_elf_generic_reloc, /* special_function */
-	 "R_RISCV_TPREL_HI20",	/* name */
-	 TRUE,			/* partial_inplace */
-	 0,			/* src_mask */
+  HOWTO (R_RISCV_TPREL_HI20,		/* type */
+	 0,				/* rightshift */
+	 2,				/* size */
+	 32,				/* bitsize */
+	 FALSE,				/* pc_relative */
+	 0,				/* bitpos */
+	 complain_overflow_signed,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,		/* special_function */
+	 "R_RISCV_TPREL_HI20",		/* name */
+	 TRUE,				/* partial_inplace */
+	 0,				/* src_mask */
 	 ENCODE_UTYPE_IMM (-1U),	/* dst_mask */
 	 FALSE),		/* pcrel_offset */
 
