8a94784bddb678e572c75927237f19492c7c0b65
Andrew Waterman <waterman@cs.berkeley.edu>
2015-10-26 14:47:43 -0700

    gcc: don't enable -msave-restore by default for -Os
    It's still flaky with respect to DWARF and so should be considered
    experimental.

--- gcc/gcc/common/config/riscv/riscv-common.c
+++ gcc/gcc/common/config/riscv/riscv-common.c
@@ -121,7 +121,6 @@ static const struct default_options riscv_option_optimization_table[] =
   {
     { OPT_LEVELS_1_PLUS, OPT_fsection_anchors, NULL, 1 },
     { OPT_LEVELS_1_PLUS, OPT_fomit_frame_pointer, NULL, 1 },
-    { OPT_LEVELS_SIZE, OPT_msave_restore, NULL, 1 },
     { OPT_LEVELS_NONE, 0, NULL, 0 }
   };
 
