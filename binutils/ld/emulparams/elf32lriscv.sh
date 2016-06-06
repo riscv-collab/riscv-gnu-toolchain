15d2d6f9daefef6c3bf7a442285e15d84bcca938
Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
2016-06-07 00:16:59 +0100

    Add correct ELF_INTERPRETER_NAME to binutils/ld/emulparams/elf{32,64}riscv.sh
    If not set, the dynamic linker is set to "/lib/ld.so.1"

--- binutils/ld/emulparams/elf32lriscv.sh
+++ binutils/ld/emulparams/elf32lriscv.sh
@@ -1,2 +1,3 @@
 . ${srcdir}/emulparams/elf32lriscv-defs.sh
 OUTPUT_FORMAT="elf32-littleriscv"
+ELF_INTERPRETER_NAME=\"/lib/ld-linux-riscv64ifd_ilp32.so.1\"
--- binutils/ld/emulparams/elf64lriscv.sh
+++ binutils/ld/emulparams/elf64lriscv.sh
@@ -1,2 +1,3 @@
 . ${srcdir}/emulparams/elf64lriscv-defs.sh
 OUTPUT_FORMAT="elf64-littleriscv"
+ELF_INTERPRETER_NAME=\"/lib/ld-linux-riscv64ifd_lp64.so.1\"
