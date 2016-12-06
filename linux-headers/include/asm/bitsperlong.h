805ee1d3bf5e10e0f4bb91841bf6bf56e7009997
Andrew Waterman <andrew@sifive.com>
2016-12-06 15:45:35 -0800

    avoid non-standard predefined macros

--- linux-headers/include/asm/bitsperlong.h
+++ linux-headers/include/asm/bitsperlong.h
@@ -1,7 +1,7 @@
 #ifndef _ASM_RISCV_BITSPERLONG_H
 #define _ASM_RISCV_BITSPERLONG_H
 
-#define __BITS_PER_LONG _RISCV_SZLONG
+#define __BITS_PER_LONG (__SIZEOF_POINTER__ * 8)
 
 #include <asm-generic/bitsperlong.h>
 
