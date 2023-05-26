3ea87a1b928238fefff09c904cdb446e25c83587
Patrick O'Neill <patrick@rivosinc.com>
2023-05-26 09:42:51 -0700

    Add github action ci to make-report using the spike simulator

--- .github/setup-apt.sh
+++ .github/setup-apt.sh
@@ -5,4 +5,5 @@ dpkg --add-architecture i386
 apt update
 apt install -y autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev \
             libgmp-dev gawk build-essential bison flex texinfo gperf libtool \
-            patchutils bc zlib1g-dev libexpat-dev git ninja-build expect
+            patchutils bc zlib1g-dev libexpat-dev git ninja-build expect \
+            device-tree-compiler
