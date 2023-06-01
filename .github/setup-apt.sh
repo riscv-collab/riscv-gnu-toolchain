9bd829ed9ac8b776e69a6b7cfec181c787eefd80
alexsifivetw <alex.chiang@sifive.com>
2023-06-01 05:41:12 -0700

    Fix typo

--- .github/setup-apt.sh
+++ .github/setup-apt.sh
@@ -5,4 +5,4 @@ dpkg --add-architecture i386
 apt update
 apt install -y autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev \
             libgmp-dev gawk build-essential bison flex texinfo gperf libtool \
-            patchutils bc zlib1g-dev libexpat-dev git ninja-build cmake ibglib2.0-dev expect
+            patchutils bc zlib1g-dev libexpat-dev git ninja-build cmake libglib2.0-dev expect
