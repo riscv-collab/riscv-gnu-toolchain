2951d862f5f6030db9b79ee654afe66748224359
Patrick O'Neill <patrick@rivosinc.com>
2023-07-20 14:25:50 -0700

    Add get-most-recent-ci-hash.py helper script

---
+++ scripts/get-most-recent-ci-hash.py
@@ -0,0 +1,62 @@
+# Gets the most recent hash that has an associated CI run
+
+import os
+import argparse
+from typing import List
+from github import Auth, Github
+
+def gcc_hashes(hash: str, subsequent: bool):
+    """ Get the most recent GCC hashes within a 100 commits (in order from closest to furthest)"""
+    if subsequent is False: # Get prior commit
+        old_commit = os.popen(f'cd gcc && git checkout master --quiet && git pull --quiet && git rev-parse {hash}~100').read().strip()
+        print(f'git rev-list --format=short --ancestry-path {old_commit}~1..{hash}~1')
+        commits = os.popen(f'cd gcc && git rev-list --ancestry-path {old_commit}^..{hash}^').read()
+        commits = commits.splitlines()
+    else:
+        os.popen('cd gcc && git checkout master --quiet && git pull --quiet').read()
+        commits = os.popen(f'cd gcc && git rev-list --ancestry-path {hash}..HEAD | head -100').read()
+        commits = list(reversed(commits.splitlines()))
+
+    return commits
+
+def get_valid_hash(hashes: List[str], token: str):
+    auth = Auth.Token(token)
+    g = Github(auth=auth)
+
+    repo = g.get_repo('patrick-rivos/riscv-gnu-toolchain')
+
+    for hash in hashes:
+        # Arbitrarily use gcc-newlib-rv32gc-ilp32d. It seems to always build.
+        artifact_name = f'gcc-newlib-rv32gc-ilp32d-{hash}-non-multilib-report.log'
+        artifacts = repo.get_artifacts(artifact_name).get_page(0)
+        if len(artifacts) != 0:
+            return hash
+
+    return "No valid hash"
+
+def main(hash: str, subsequent: bool, token: str):
+    commits = gcc_hashes(hash, subsequent)
+    print(get_valid_hash(commits, token))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get closest valid GCC hash")
+    parser.add_argument(
+        "-hash",
+        required=True,
+        type=str,
+        help="Commit hash of GCC to get artifacts for",
+    )
+    parser.add_argument(
+        "-subsequent",
+        action='store_true',
+        help="Get the subsequent closest commit. Defaults to prior.",
+    )
+    parser.add_argument(
+        "-token",
+        required=True,
+        type=str,
+        help="Github access token.",
+    )
+    args = parser.parse_args()
+
+    main(args.hash, args.subsequent, args.token)
