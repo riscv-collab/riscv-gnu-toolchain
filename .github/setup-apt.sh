43536acae8791de5fc93acad51d0c03dda9f903e
Osman Karaketir <osmankaraketir@gmail.com>
2024-12-15 23:19:30 +0300

    add `python3-tomli` to `setup-apt.sh`

--- .github/setup-apt.sh
+++ .github/setup-apt.sh
@@ -3,7 +3,7 @@
 # install OS prerequisites
 dpkg --add-architecture i386
 apt update
-apt install -y autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev \
+apt install -y autoconf automake autotools-dev curl python3 python3-pip python3-tomli libmpc-dev libmpfr-dev \
             libgmp-dev gawk build-essential bison flex texinfo gperf libtool \
             patchutils bc zlib1g-dev libexpat-dev git ninja-build cmake libglib2.0-dev expect \
             device-tree-compiler python3-pyelftools libslirp-dev
